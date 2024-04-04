636862e4d17d912faafff932d07a9086374942a8
Kyra <khamerling-potts@wustl.edu>
2024-04-04 13:57:52 -0400

    first commit. basic app testing fetch with heroku api

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  usersToDisplay = users.map((user) => <Text key={user.id}>{user.name}</Text>);
+
+  useEffect(() => {
+    fetch(
+      "https://healthseed-flask-backend-94c8efc27481.herokuapp.com/users"
+    ).then((r) => {
+      if (r.ok) {
+        r.json().then((data) => setUsers(data));
+      }
+    });
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {usersToDisplay ? usersToDisplay : "No users"}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
